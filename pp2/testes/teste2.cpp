e05997f22ca0493e7f2e57e1b8e592a53e95d263
guilhermemonte31 <120525923+guilhermemonte31@users.noreply.github.com>
2023-12-26 19:57:34 -0400

    Update teste2.cpp

--- pp2/testes/teste2.cpp
+++ pp2/testes/teste2.cpp
@@ -11,15 +11,18 @@ int main(){
     char linha = pos[0];
     int coluna = pos[1] - 48;
     pair<char, char> inicio(linha, coluna);
-    vector<pair<char, char>> movimentos = {{-2, -1}, {-2, 1}, {-1, 2}, {-1, -2}, {1, 2}, {1, -2}, {2, 1}, {2, -1}};
+    vector<pair<char, int>> movimentos = {{char(linha-2), (coluna-1)}, {char(linha-2), (coluna+1)}, {char(linha-1), (coluna+2)}, {char(linha-1), (coluna-2)}, {char(linha+1), (coluna+2)}, {char(linha+1), (coluna-2)}, {char(linha+2), (coluna+1)}, {char(linha+2), (coluna-1)}};
 
     for(int i=0; i<8; i++){
-        char l = abs(linha - movimentos[i].first);
-        int c = abs(coluna - movimentos[i].second);
-        if(l>='a'||l<='h'||c>=1||c<=8){
-            cout<<l<<c;
+        if(movimentos[i].first >= 'a' && movimentos[i].first<='h'&& movimentos[i].second>=1 && movimentos[i].second<=8){
+           cout<<movimentos[i].first<<movimentos[i].second;
             cout<<endl;
-        }
+        }  
+        
     }
+
+
+
+
     
 }
