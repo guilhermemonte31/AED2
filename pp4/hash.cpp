214b1ed6e816325d8a4be8c4363ee71ca6bb26da
guilhermemonte31 <120525923+guilhermemonte31@users.noreply.github.com>
2024-02-07 17:59:53 -0400

    Update hash.cpp

--- pp4/hash.cpp
+++ pp4/hash.cpp
@@ -5,16 +5,17 @@
 
 using namespace std;
 
-
 class HashTable{
     private:
         unsigned int table_size;
         list<pair<char, string>> *table;
+        char *letters_vector;
     public:
         HashTable(unsigned int);
         ~HashTable();
         int perfect_hash(string);
         void insert_value(char, string);
+        char search_letter(string);
         void show();
 
 };
@@ -22,6 +23,46 @@ class HashTable{
 HashTable::HashTable(unsigned int n){
     table_size = n;
     table = new list<pair<char, string>>[table_size];
+    
+
+    insert_value('A', ":::");
+    insert_value('B', ".::");
+    insert_value('C', ":.:");
+    insert_value('D', "::.");
+    insert_value('E', ":..");
+    insert_value('F', ".:.");
+    insert_value('G', "..:");
+    insert_value('H', "...");
+    insert_value('I', "|::");
+    insert_value('J', ":|:");
+    insert_value('K', "::|");
+    insert_value('L', "|.:");
+    insert_value('M', ".|:");
+    insert_value('N', ".:|");
+    insert_value('O', "|:.");
+    insert_value('P', ":|.");
+    insert_value('Q', ":.|");
+    insert_value('R', "|..");
+    insert_value('S', ".|.");
+    insert_value('T', "..|");
+    insert_value('U', ".||");
+    insert_value('V', "|.|");
+    insert_value('W', "||.");
+    insert_value('X', "-.-");
+    insert_value('Y', ".--");
+    insert_value('Z', "--.");
+    insert_value(' ', "---");
+    insert_value('.', "~~~");
+
+    char letter = 'A';
+    letters_vector = new char[28];
+    for(int i=0; i<26; i++){
+        letters_vector[i] = letter;
+        letter++;
+    }
+    letters_vector[26] = ' ';
+    letters_vector[27] = '.';
+    
 }
 
 HashTable::~HashTable(){
@@ -32,6 +73,7 @@ HashTable::~HashTable(){
     table= nullptr;
     table_size=0;
 }
+
 int HashTable::perfect_hash(string key){
     int TAM_ASCII = 256;
     int M = 7; // tamanho da tabela
@@ -45,7 +87,6 @@ int HashTable::perfect_hash(string key){
 
 void HashTable::insert_value(char letter, string code){
     int id = perfect_hash(code);
-    cout<<"teste "<<letter<<" "<<id<<endl;
     pair <char, string> test;
     test.first = letter;
     test.second = code;
@@ -63,44 +104,44 @@ void HashTable::show(){
     }
 }
 
-void create_cryptography(HashTable *ht){
-
-
-    ht->insert_value('A', ":::");
-    ht->insert_value('B', ".::");
-    ht->insert_value('C', ":.:");
-    ht->insert_value('D', "::.");
-    ht->insert_value('E', ":..");
-    ht->insert_value('F', ".:.");
-    ht->insert_value('G', "..:");
-    ht->insert_value('H', "...");
-    ht->insert_value('I', "|::");
-    ht->insert_value('J', ":|:");
-    ht->insert_value('K', "::|");
-    ht->insert_value('L', "|.:");
-    ht->insert_value('M', ".|:");
-    ht->insert_value('N', ".:|");
-    ht->insert_value('O', "|:.");
-    ht->insert_value('P', ":|.");
-    ht->insert_value('Q', ":.|");
-    ht->insert_value('R', "|..");
-    ht->insert_value('S', ".|.");
-    ht->insert_value('T', "..|");
-    ht->insert_value('U', ".||");
-    ht->insert_value('V', "|.|");
-    ht->insert_value('W', "||.");
-    ht->insert_value('X', "-.-");
-    ht->insert_value('Y', ".--");
-    ht->insert_value('Z', "--.");
-    ht->insert_value(' ', "---");
-    ht->insert_value('.', "~~");
+char HashTable::search_letter(string code){
+    int id = perfect_hash(code);
+    for(auto it = table[id].begin(); it != table[id].end(); it++){
+        if((*it).second == code){
+            return (*it).first;
+            
+        }
+    }
 }
+
+
 
 
 int main(){
 
+    //creating hash table
     unsigned int hash_table_size = 7;
     HashTable ht{hash_table_size};
-    create_cryptography(&ht);
-    //ht.show();
+
+
+    // //vector of alphabet elements
+    // char letters_vector[28] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ','.'};
+
+
+    // string input_string = "--.::.:..---...|.|||....|:.::.---..|...|.||.||.|---||.|..-.-.|||..---|:.:.:::.-.-|.||::----.-|:.::.|.|.--..|..|.|::::---|.:|.:---::.:|....::..:.::.---|.:::...:|::.:.|..---|.:|.::.|||.....:.|..---.--::.::.---...|.|||..||...|:.::.--.";
+
+
+    // //getting last letter
+    // int s = input_string.size();
+    // string w;
+    // w+= input_string[s-3];
+    // w+= input_string[s-2];
+    // w+= input_string[s-1];
+    // int letter_w = ht.search_letter(w);
+
+    // //getting value of k
+    // int k;
+
+
+
 }
